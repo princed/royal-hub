22c32791f31919dda435a43a93a1505a61ade6ee
Eugene Datsky <eugene@datsky.ru>
2014-10-24 12:48:11 +0200

    Mising files and reformat app.js again

--- www/js/app.js
+++ www/js/app.js
@@ -14,99 +14,99 @@ angular.module('starter', [
   'angular-jwt'
 ])
 
-.run(function($ionicPlatform, auth) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-
-  // This hooks al auth events to check everything as soon as the app starts
-  auth.hookEvents();
-})
-
-.config(function($stateProvider, $urlRouterProvider, authProvider) {
-
-  authProvider.init({
-    domain: 'royal-hub.auth0.com',
-    clientID: 'qdPCQ9Ksvdz6weZUIbebl5bltmfqqKwi',
-    loginState: 'login'
-  });
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html',
-      controller: 'LoginCtrl'
-    })
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html",
-      data: {
-        // This tells Auth0 that this state requires the user to be logged in.
-        // If the user isn't logged in and he tries to access this state
-        // he'll be redirected to the login page
-        requiresLogin: true
+  .run(function ($ionicPlatform, auth) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
-      }
-    })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
     });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // This hooks al auth events to check everything as soon as the app starts
+    auth.hookEvents();
+  })
 
-});
+  .config(function ($stateProvider, $urlRouterProvider, authProvider) {
+
+    authProvider.init({
+      domain: 'royal-hub.auth0.com',
+      clientID: 'qdPCQ9Ksvdz6weZUIbebl5bltmfqqKwi',
+      loginState: 'login'
+    });
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      })
+
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html",
+        data: {
+          // This tells Auth0 that this state requires the user to be logged in.
+          // If the user isn't logged in and he tries to access this state
+          // he'll be redirected to the login page
+          requiresLogin: true
+        }
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
+        }
+      })
+
+      .state('tab.friends', {
+        url: '/friends',
+        views: {
+          'tab-friends': {
+            templateUrl: 'templates/tab-friends.html',
+            controller: 'FriendsCtrl'
+          }
+        }
+      })
+      .state('tab.friend-detail', {
+        url: '/friend/:friendId',
+        views: {
+          'tab-friends': {
+            templateUrl: 'templates/friend-detail.html',
+            controller: 'FriendDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
+  });
 
