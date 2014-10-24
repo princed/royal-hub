e7dbfe06eb0a5349ef9f058a14387ce43807e6d2
Evgenii Schepotiev <evgenii.schepotiev@jetbrains.com>
2014-10-24 14:25:04 +0200

    pages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,20 @@
 angular.module('starter.controllers', ['restangular', 'starter.services'])
 
   .controller('DashCtrl', function ($scope, github) {
-    github.getMyEvents().then(function(events) {
-      $scope.user = events;
+    $scope.page = 1;
+    github.getMyEvents().then(function (events) {
+      $scope.events = events;
+      var page = function(page) {
+        $scope.events.getList({page: page}).then(function (events) {
+          $scope.events = events;
+        });
+      };
+      $scope.nextPage = function () {
+        page(++$scope.page);
+      };
+      $scope.prevPage = function () {
+        page(--$scope.page);
+      };
     });
   })
 
@@ -14,25 +26,25 @@ angular.module('starter.controllers', ['restangular', 'starter.services'])
     $scope.friend = Friends.get($stateParams.friendId);
   })
 
-  .controller('LoginCtrl', function($scope, auth, store, $location) {
-    $scope.login = function() {
+  .controller('LoginCtrl', function ($scope, auth, store, $location) {
+    $scope.login = function () {
       auth.signin({
         authParams: {
           scope: 'openid offline_access',
           device: 'Mobile device'
         }
-      }, function(profile, token, accessToken, state, refreshToken) {
+      }, function (profile, token, accessToken, state, refreshToken) {
         // Success callback
         store.set('profile', profile);
         store.set('token', token);
         store.set('refreshToken', refreshToken);
         $location.path('/');
-      }, function() {
+      }, function () {
         // Error callback
       });
     }
   })
 
-.controller('AccountCtrl', function($scope, auth) {
+  .controller('AccountCtrl', function ($scope, auth) {
     $scope.auth = auth
-});
+  });
